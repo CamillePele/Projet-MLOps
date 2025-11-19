fafb8ae8d04c9ef599c4ff52b301b8f053cfbf2b
Camille <camille.pele03@gmail.com>
2025-11-19 19:42:06 +0100

    fix: first start errors

--- apps/front/vite.config.ts
+++ apps/front/vite.config.ts
@@ -1,19 +1,12 @@
-import path from "path"
-import { fileURLToPath } from "url"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  plugins: [react(), tailwindcss(), tsconfigPaths()],
   server: {
     watch: {
       usePolling: true,
